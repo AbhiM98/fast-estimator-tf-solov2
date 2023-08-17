512fb2326ae23119758a889ab4df8e211d8ab9b7
Abhilash Mane <abhilash.mane@sentera.com>
2023-08-17 15:06:38 -0500

    Added tensorflow solov2 with resnet 101 and aws dataloader

---
+++ installation_docs/mac_installation.md
@@ -0,0 +1,115 @@
+# MacOS Installation
+
+This is a guide for how to install FastEstimator on MacOS starting from a brand new machine. If you already have some of the dependencies that are mentioned here, you can skip down to where the steps start becoming relevant for you.
+
+## Step 0: Install Homebrew
+
+If you don't have brew installed yet, you can install it following the instructions [here](https://docs.brew.sh/Installation).
+
+## Step 1: Install System Dependencies
+
+```bash
+brew update && brew install pyenv pyenv-virtualenv
+```
+
+```bash
+echo 'eval "$(pyenv init --path)"' >> ~/.zprofile
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshrc
+```
+
+```bash
+brew install zlib sqlite bzip2 libiconv libzip openssl rust
+```
+
+The following graphviz and mactex dependencies are kind of large (~5GB), so you can skip them if you don't want to use [auto-doc generation features](https://github.com/fastestimator/fastestimator/blob/master/tutorial/advanced/t10_report_generation.ipynb) like [Traceability reports](https://github.com/fastestimator/fastestimator/blob/master/tutorial/resources/t10a_traceability.pdf). You can also choose to install them later if you change your mind.
+
+```bash
+brew install graphviz
+brew install --cask mactex  # You could alternatively get this from https://www.tug.org/mactex/mactex-download.html
+eval "$(/usr/libexec/path_helper)"  # You can skip this line if you instead restart your terminal window
+```
+
+## Step 2: Install Miniforge
+
+```bash
+CFLAGS="-I$(brew --prefix openssl)/include -I$(brew --prefix bzip2)/include -I$(brew --prefix readline)/include -I$(brew --prefix zlib)/include -I$(brew --prefix sqlite)/include -I$(xcrun --show-sdk-path)/usr/include" LDFLAGS="-L$(brew --prefix openssl)/lib -L$(brew --prefix bzip2)/lib -L$(brew --prefix readline)/lib -L$(brew --prefix zlib)/lib -L$(brew --prefix sqlite)/lib" pyenv install miniforge3-4.14.0-2
+```
+
+```bash
+pyenv global miniforge3-4.14.0-2
+```
+
+## Step 3: Create a python environment
+
+```bash
+conda update --force conda -y
+```
+
+```bash
+conda create -n FE16 python=3.8 -y
+```
+
+Use pyenv rather than conda to activate your virtual environment to ensure that both pip and conda will point to the same python environment:
+
+```bash
+pyenv activate FE16
+```
+
+## Step 4: Install python dependencies
+
+```bash
+conda install -c apple tensorflow-deps==2.10.0 -y
+```
+
+```bash
+python -m pip install tensorflow-macos==2.11.0
+```
+
+```bash
+python -m pip install tensorflow-metal==0.7.1
+```
+
+```bash
+python -m pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2
+```
+
+```bash
+conda install qtconsole -y
+```
+
+```bash
+conda install jupyter -y
+```
+
+```bash
+conda install -c conda-forge jupyterlab -y
+```
+
+Jupyterlab smuggles in a different version of urllib3 which we need to get rid of to prevent some warning messages later:
+
+```bash
+pip uninstall -y urllib3 && pip install urllib3==1.26.16
+```
+
+## Step 5: Install FastEstimator
+
+For regular usage:
+
+```bash
+pip install fastestimator
+```
+
+For FE developers:
+
+```bash
+git clone https://github.com/fastestimator/fastestimator.git
+```
+
+```bash
+cd fastestimator
+```
+
+```bash
+pip install -e .
+```
---
+++ test/run_nightly_build.sh
@@ -0,0 +1,134 @@
+#!/usr/bin/env bash
+
+# Copyright 2020 The FastEstimator Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============================================================================
+full_path="$(realpath "$0")"
+dir_path="$(dirname "$full_path")"
+
+declare -A exectime
+declare -A result
+
+# run apphub
+for file in $(find "$dir_path"/apphub_scripts -type f); do
+    if [[ $file == *.sh ]]; then
+        if [[ $file == *template* ]]; then
+            continue
+        fi
+        echo "$file"
+
+        # sleep (fix 'Kernel died' issue: https://github.com/nteract/papermill/issues/511)
+        sleep 10
+
+        start=$(date +%s)
+        bash "$file"
+        result[$file]=$?
+        end=$(date +%s)
+
+        # sleep (fix 'Kernel died' issue: https://github.com/nteract/papermill/issues/511)
+        sleep 10
+
+        # clean GPU memory
+        if ls /dev/nvidia* 1> /dev/null 2>&1; then
+            for i in $(lsof /dev/nvidia* | awk 'FNR>1 {print $2}' | sort -u); do
+                kill -9 "$i";
+            done
+        fi
+
+        # clean cache (fix issue of dataloader being killed)
+        rm -r /root/.cache
+
+        # sleep (fix issue of kernel die)
+        sleep 10
+
+        exectime[$file]=$((end-start))
+        if [ ! ${result[$file]} -eq 0 ]; then
+            echo "---------------- error log of $file-------------------"
+            cat "${file/'.sh'/'_stderr.txt'}"
+            echo "------------------------------------------------------"
+        fi
+    fi
+done
+
+rm -rf "$dir_path"/tutorial
+cp -r "$(realpath "$dir_path"/../tutorial)" "$dir_path"
+
+# run tutorial
+for nb_in in $(find "$dir_path"/tutorial -type f); do
+    if [[ $nb_in == *.ipynb ]]; then
+        echo "$nb_in"
+
+        # sleep (fix 'Kernel died' issue: https://github.com/nteract/papermill/issues/511)
+        sleep 10
+
+        nb_out="${nb_in/'.ipynb'/'_out.ipynb'}"
+        current_dir="$(dirname "$nb_in")"
+        stderr_file="${nb_in/'.ipynb'/'_stderr.txt'}"
+        start=$(date +%s)
+        papermill "$nb_in" "$nb_out" -k nightly_build 2>> "$stderr_file" --cwd "$current_dir"
+        result[$nb_in]=$?
+        end=$(date +%s)
+
+        # sleep (fix 'Kernel died' issue: https://github.com/nteract/papermill/issues/511)
+        sleep 10
+
+        # clean GPU memory
+        if ls /dev/nvidia* 1> /dev/null 2>&1; then
+            for i in $(lsof /dev/nvidia* | awk 'FNR>1 {print $2}' | sort -u); do
+                kill -9 "$i";
+            done
+        fi
+
+        # clean cache (fix issue of dataloader being killed)
+        rm -r /root/.cache
+
+        # sleep (fix 'Kernel died' issue: https://github.com/nteract/papermill/issues/511)
+        sleep 10
+
+        exectime[$nb_in]=$((end-start))
+        if [ ! ${result[$nb_in]} -eq 0 ]; then
+            echo "---------------- error log of $nb_in-------------------"
+            cat "${nb_in/'.ipynb'/'_stderr.txt'}"
+            echo "-------------------------------------------------------"
+        fi
+
+    fi
+done
+
+# print report
+echo "------------------------ report ------------------------"
+for key in "${!exectime[@]}"; do
+    if [ ${result[$key]} -eq 0 ]; then
+        echo "$key: pass, (spend ${exectime[$key]} seconds)"
+    else
+        echo "$key: fail, (spend ${exectime[$key]} seconds)"
+    fi
+done
+
+# print fail list
+echo "---------------------- fail list -----------------------"
+is_fail=0
+for key in "${!exectime[@]}"; do
+    if [ ! ${result[$key]} -eq 0 ]; then
+        echo "$key"
+        is_fail=1
+    fi
+done
+
+if [ $is_fail -eq 0 ]; then
+    echo "all tests passed"
+    exit 0
+else
+    exit 1
+fi
